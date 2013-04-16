101a5a64302fcd444029610b5badb9155bd8da0c
Sharon Lin <itsaviogiuseppe@gmail.com>
2013-04-17 01:15:37 +0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shenbridgeapp::Application.config.secret_token = '6b05a88bebccf33d2c3214abee219c271bd691352a9543fed95a9fa5af6ffe1ce815d1e7f6ee3f826269427f3b21882202c30fac06a4e998451a47c22e0c1785'
